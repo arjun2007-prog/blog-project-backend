5433f62800d581d68188a8944aed76965c05c514
arjun2007-prog <71623186+arjun2007-prog@users.noreply.github.com>
2021-01-04 09:32:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const lodash = require("lodash")
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -10,39 +11,41 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 const posts = [];
 const app = express();
 
+mongoose.connect("mongodb://localhost:27017/blogDB" , { useNewUrlParser : true , useUnifiedTopology : true });
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+//creating a schema for our collection present in blogDB
+const blogSchema =  {
+  blogName:String,
+  blogContent:String
+}
+
+const BlogData = mongoose.model("BlogData", blogSchema);
+
 app.get("/",(req,res)=>{
-  res.render("home.ejs",{
-    homeHeading:homeStartingContent,
-    postsDisplay:posts,
-  });
+
+  BlogData.find((err,dataFound)=>{
+    res.render("home.ejs",{
+      homeHeading:homeStartingContent,
+      postsDisplay:dataFound,
+    });
+  })
+ 
 });
 
 app.get("/posts/:blogName",(req,res)=>{
  //here we are using a parameter in the url the user can put any keyword they want
-  let blogParameter = lodash.lowerCase(req.params.blogName);//here we are taking the parameter what the user entered 
+  let blogParameter = lodash.lowerCase(req.params.blogName);
+  blogParameter =lodash.capitalize(blogParameter);
+  //here we are taking the parameter what the user entered 
   //and that dtring the user entered in is converted to lower case and this also ignores the "-" in the name.
-
-  for (let i = 0; i < posts.length; i++) {
-   
-    //now the post contain all the blogs as object which contain 2 properties a title and and content
-    let actualBlogTitle = posts[i].userTitle;
-
-    actualBlogTitle = lodash.lowerCase(actualBlogTitle)
-
-    if (blogParameter == actualBlogTitle) {
-
-      res.render("postsBlog.ejs",{
-        blogPageTitle:posts[i].userTitle,
-        blogPageBody:posts[i].userBlog
-      });
-      break;
-    }
-  }
+  BlogData.findOne({blogName:blogParameter},(err,dataFound)=>{;
+    res.render("postsBlog",{ blogPageTitle : dataFound.blogName , blogPageBody : dataFound.blogContent });
+  })
 
 })
 
@@ -63,14 +66,31 @@ app.get("/compose",(req,res)=>{
 });
 
 app.post("/compose",(req,res)=>{
-  let post = {
-     userBlog : req.body.newBlog,
-     userTitle : req.body.newTitle
-  }
-  posts.push(post);
-  res.redirect("/")
   
-})
+  const newBlogName = lodash.capitalize(req.body.newTitle);
+  
+  let post = new BlogData({
+     blogName : newBlogName,
+     blogContent : req.body.newBlog,
+  });
+  post.save(()=>{
+    res.redirect("/");
+  });
+  
+  
+});
+
+app.post("/postBlog",(req,res)=>{
+  if (req.body.postbutton == "goback") {
+    res.redirect("/");
+  }
+  else{
+    BlogData.deleteOne({ blogName : req.body.postbutton },(err)=>{
+      res.redirect("/");
+    });
+      
+  }
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
